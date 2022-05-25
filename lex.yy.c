a8e92c231ba26128bf21fd495af060ed6e84e118
Atharva Patil <atharvapatil@Atharvas-MacBook-Air.local>
2022-05-25 11:20:23 +0530

    added p3b

--- lex.yy.c
+++ lex.yy.c
@@ -361,9 +361,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[17] =
     {   0,
-        0,    0,    4,    3,    2,    1,    2,    1,    0
+        0,    0,    4,    3,    3,    3,    0,    0,    0,    0,
+        0,    0,    0,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -372,16 +373,16 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    1,    1,    1,
-        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    2,    1,    3,    1,
 
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    4,    1,    1,    5,    1,    1,    1,    1,    6,
+        1,    7,    1,    8,    9,   10,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,31 +399,35 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[11] =
     {   0,
-        1,    1,    1
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[18] =
     {   0,
-        0,    0,    8,    9,    2,    4,    0,    0,    9
+        0,    3,   19,   20,   10,   14,   11,   13,    8,    7,
+        1,    1,    0,   20,   20,   20,    0
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[18] =
     {   0,
-        9,    1,    9,    9,    9,    9,    5,    6,    0
+       17,   17,   16,   16,   16,   16,   16,   16,   16,   16,
+       16,   16,   16,   16,   16,    0,   16
     } ;
 
-static const flex_int16_t yy_nxt[13] =
+static const flex_int16_t yy_nxt[31] =
     {   0,
-        4,    5,    6,    7,    7,    8,    8,    9,    3,    9,
-        9,    9
+        4,   16,   16,   15,   14,   16,    5,   16,    6,    5,
+       13,    6,   12,   11,   10,    9,    8,    7,   16,    3,
+       16,   16,   16,   16,   16,   16,   16,   16,   16,   16
     } ;
 
-static const flex_int16_t yy_chk[13] =
+static const flex_int16_t yy_chk[31] =
     {   0,
-        1,    1,    1,    5,    5,    6,    6,    3,    9,    9,
-        9,    9
+       17,    0,    0,   13,   12,    0,    1,    0,    1,    2,
+       11,    2,   10,    9,    8,    7,    6,    5,    3,   16,
+       16,   16,   16,   16,   16,   16,   16,   16,   16,   16
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,12 +444,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "p1b.l"
-#line 2 "p1b.l"
+#line 1 "p3b.l"
+#line 2 "p3b.l"
 #include<stdio.h>
-int i=0;
-#line 446 "lex.yy.c"
-#line 447 "lex.yy.c"
+int sf=0,pf=0;
+#line 451 "lex.yy.c"
+#line 452 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -661,9 +666,9 @@ YY_DECL
 		}
 
 	{
-#line 7 "p1b.l"
+#line 5 "p3b.l"
 
-#line 666 "lex.yy.c"
+#line 671 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -690,13 +695,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 17 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 9 );
+		while ( yy_base[yy_current_state] != 20 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,20 +727,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "p1b.l"
-{i++;}
+#line 6 "p3b.l"
+{sf++; fprintf(yyout,"readf");}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 9 "p1b.l"
-{i;}
+#line 7 "p3b.l"
+{pf++; fprintf(yyout,"writef");}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 10 "p1b.l"
+#line 8 "p3b.l"
 ECHO;
 	YY_BREAK
-#line 738 "lex.yy.c"
+#line 743 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1032,7 +1037,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 17 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1060,11 +1065,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 17 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 16);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1740,18 +1745,18 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "p1b.l"
+#line 8 "p3b.l"
 
 int yywrap()
 {
-  return 1;
-
+ return 1;   
 }
 int main()
 {
-yyin=fopen("p1b.txt","r");
+yyin=fopen("3b1.txt","r");
+yyout=fopen("3b2.txt","w");
 yylex();
-printf("Number of identifiers = %d\n", i);
-return 1;
+printf("no of scanf =%d\n no of printf =%d\n",sf,pf);
+return 0;
 }
 
